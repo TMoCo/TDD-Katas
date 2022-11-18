c27d06d7ccf3b32e7f092dbbcdd4cb5dd74cbaf1
Thomas Moreno Cooper <thomas.cooper@bbc.co.uk>
2022-11-18 18:04:41 +0000

    refactor add

--- string_calculator/src/add.js
+++ string_calculator/src/add.js
@@ -8,40 +8,69 @@ const convertStringToNumber = (numberAsString) => {
   return num > 1000 ? 0 : num;
 };
 
-const extractDelimitersFromString = (string) => {
-  let delimiters;
-  let delimitersString;
+const parseDelimiters = (inputString) => {
+  if (inputString[0] === "[") {
+    const endingIndex = inputString.search("]");
 
-  if (string[0] !== "[") delimitersString = "[:;,\n]";
-  else {
-    const endingIndex = string.search("]");
-    delimitersString = "[" + string.substring(1, endingIndex) + "]";
-    string = string.substring(endingIndex + 1, string.length);
+    if (endingIndex === -1) {
+      throw new Error("Invalid string: could not find delimiters");
+    }
+
+    const delimitersString = "[" + inputString.substring(1, endingIndex) + "]";
+    return { deli };
   }
+};
 
-  delimiters = new RegExp(delimitersString);
-
-  for (let i = 0; i < string.length; ++i) {
-    if (delimitersString.includes(string[i])) {
-      if (delimitersString.includes(string[i + 1]) || i + 1 === string.length) {
+const checkIntegersString = (integers, delimiters) => {
+  for (let i = 0; i < integers.length; ++i) {
+    if (delimiters.includes(integers[i])) {
+      if (delimiters.includes(integers[i + 1]) || i + 1 === integers.length) {
         throw new Error("Delimiters must be followed by a number");
       }
     }
   }
+};
 
-  return { string, delimiters };
+const splitDelimitersFromIntegers = (inputString) => {
+  if (inputString[0] === "[") {
+    const endingIndex = inputString.search("]");
+
+    if (endingIndex === -1) {
+      throw new Error("Invalid string: could not find delimiters");
+    }
+
+    const delimitersString = "[" + inputString.substring(1, endingIndex) + "]";
+    const integers = inputString.substring(endingIndex + 1, inputString.length);
+
+    checkIntegersString(integers, delimitersString);
+
+    return {
+      string: integers,
+      delimiters: new RegExp(delimitersString),
+    };
+  } else {
+    const delimitersString =
+      inputString[0] === "[" ? parseDelimiters(inputString) : "[:;,\n]";
+
+    checkIntegersString(inputString, delimitersString);
+
+    return {
+      string: inputString,
+      delimiters: new RegExp(delimitersString),
+    };
+  }
 };
 
 const add = (inputString) => {
   if (inputString.length == 0) return 0;
 
-  const { string, delimiters } = extractDelimitersFromString(inputString);
-  const numbers = string.split(delimiters);
+  const { string, delimiters } = splitDelimitersFromIntegers(inputString);
 
-  let sum = 0;
-  for (let i = 0; i < numbers.length; ++i) {
-    sum += convertStringToNumber(numbers[i]);
-  }
+  const integers = string.split(delimiters);
+  const sum = integers.reduce(
+    (total, current) => total + convertStringToNumber(current),
+    0
+  );
 
   return sum;
 };
