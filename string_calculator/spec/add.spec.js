25ed41e4a38a9a763f46f22e7f19db9027f5d3df
Thomas Moreno Cooper <thomas.cooper@bbc.co.uk>
2022-11-18 16:38:28 +0000

    all tests passing, start refactor

--- string_calculator/spec/add.spec.js
+++ string_calculator/spec/add.spec.js
@@ -38,35 +38,7 @@ describe("Add functionality", () => {
     }).toThrow(new Error("Delimiters must be followed by a number"));
   });
 
-  fit("Splits the string into numbers using delimiters spedified at the start of the string", () => {
+  it("Splits the string into numbers using delimiters spedified at the start of the string", () => {
     expect(add("[|{]1{4|2")).toEqual(7);
   });
 });
-
-/*
-
-eg: "[:;'\|]12|23"
-
-General Info: 
-- The add functions take a string
-- Each string contains zero or more integer values
-- Separators are used between values
-Test Cases:
-[x] Floating point numbers should be converted to integers
-[x] Separators can be , or ; or : or \n
- -> For example, “1,2” or “3\n4” or “3;4”
-[x] These can be added together
-[x] An empty string will return zero
-[x] A string with a single integer will return that value
-[x] Numbers bigger than 1,000 should be ignored,
- -> for example: 2 + 1001 == 2
-[x] A Delimiter must be followed by a number
- -> 1\n2 and 1,2 as is 1;2 are all valid
- -> \n or 2; are not valid
-[x] Delimiters can be mixed e.g., 1,2;3
-[x] Only allow positive numbers,
- -> negative numbers generate an error
- Extension points
-n add a list of valid delimiters before the values in brackets
-p [,;]1,2;3
-*/
--- string_calculator/src/add.js
+++ string_calculator/src/add.js
@@ -1,16 +1,14 @@
 const convertStringToNumber = (numberAsString) => {
-  let num = Math.round(Number(numberAsString));
+  const num = parseInt(numberAsString);
 
-  if (num > 1000) {
-    num = 0;
-  } else if (num < 0) {
+  if (num < 0) {
     throw new Error("Negative number detected, bad.");
   }
 
-  return num;
+  return num > 1000 ? 0 : num;
 };
 
-const workThroughDelimiters = (string) => {
+const extractDelimitersFromString = (string) => {
   let delimiters;
   let delimitersString;
 
@@ -18,42 +16,12 @@ const workThroughDelimiters = (string) => {
   else {
     const endingIndex = string.search("]");
     delimitersString = "[" + string.substring(1, endingIndex) + "]";
-    string = string.substring(endingIndex, string.length);
+    string = string.substring(endingIndex + 1, string.length);
   }
 
   delimiters = new RegExp(delimitersString);
-  console.log(delimiters);
-
-  /*
-  They're working!
-  What did you do?
-
-  the regex string needs square brackets around the delimiters:
-  [:;,\n] instead of :;,\n
-
-  Oohhh good spot
-
-  The problem now is that the delimiters check starts at the beginning
-   of the string, which fails for strings like [;:]1;2:2
-I've got an idea
-   Grr
-
-   Okay we're not getting the Error now
-
-   String.split shouldnt be from the start of the string, but only split the numbers section
-  
-  We just need to remove the deliters from the front?
-
-  Yes, maybe return an object containing the modified string and the delimiters?
-
-  Good plan - i like that
-
-  Not splitting -_-
-  undefined
-   */
 
   for (let i = 0; i < string.length; ++i) {
-    console.log("string under assessment", string[i]);
     if (delimitersString.includes(string[i])) {
       if (delimitersString.includes(string[i + 1]) || i + 1 === string.length) {
         throw new Error("Delimiters must be followed by a number");
@@ -64,14 +32,11 @@ I've got an idea
   return { string, delimiters };
 };
 
-const add = (p) => {
-  if (p.length == 0) return 0;
+const add = (inputString) => {
+  if (inputString.length == 0) return 0;
 
-  const { string, d } = workThroughDelimiters(p);
-  const numbers = string.split(d);
-
-  console.log("numbers >>> ", numbers);
-  console.log(d);
+  const { string, delimiters } = extractDelimitersFromString(inputString);
+  const numbers = string.split(delimiters);
 
   let sum = 0;
   for (let i = 0; i < numbers.length; ++i) {
