bd0822903efb2fd8595f45cb28837b6f469c0d4f
Thomas Moreno Cooper <thomas.cooper@bbc.co.uk>
2022-11-25 11:44:10 +0000

    player moves basic + tests

--- tic_tac_toe/src/TicTacToe.ts
+++ tic_tac_toe/src/TicTacToe.ts
@@ -1,13 +1,28 @@
 export default class TicTacToe {
-  readonly turn: number[];
+  readonly moves: number[][];
   readonly board: string[][];
 
   constructor() {
-    this.turn = [];
+    this.moves = [];
     this.board = Array<string>(3)
       .fill('#')
       .map(() => Array<string>(3).fill('#'));
+  }
 
-    console.log(this.board);
+  getCurrentPlayerToken(): string {
+    return this.turnsPlayed() & 1 ? 'O' : 'X';
+  }
+
+  nextMove(row: number, col: number): void {
+    if (this.board[row][col] !== '#') {
+      throw new Error('Invalid move - Board place already occupied!');
+    }
+    
+    this.moves.push([row, col]);
+    this.board[row][col] = this.getCurrentPlayerToken();
+  }
+
+  turnsPlayed(): number {
+    return this.moves.length;
   }
 }
--- tic_tac_toe/test/game.test.ts
+++ tic_tac_toe/test/game.test.ts
@@ -1,14 +1,76 @@
 import assert from 'assert';
 import TicTacToe from '../src/TicTacToe';
 
+const gameStates = [
+  [
+    [0, 0],
+    [0, 1],
+  ],
+];
+
+function advanceGameToState(game: TicTacToe, state: number[][]) {
+  for (const move of state) {
+    game.nextMove(move[0], move[1]);
+  }
+}
+
 describe('Game of tic tac toe', () => {
-  it('creates an empty board', () => {
+  it('initialises a board and an array of moves', () => {
     const game = new TicTacToe();
 
-    game.board.forEach((row) => {
-      row.forEach((col) => {
-        assert.equal(col, '#');
+    assert(game.board);
+    assert(game.moves);
+  });
+
+  describe('Player moves', () => {
+    it("Processes a player's next move", () => {
+      const game = new TicTacToe();
+      advanceGameToState(game, gameStates[0]);
+
+      assert.deepEqual(game.moves[0], [0, 0]);
+      assert.deepEqual(game.moves[1], [0, 1]);
+    });
+
+    it('Keeps track of the number of turns played', () => {
+      const game = new TicTacToe();
+      advanceGameToState(game, gameStates[0]);
+
+      assert.strictEqual(game.turnsPlayed(), 2);
+    });
+
+    it('Throws an error when placing a token on an occupied board place', () => {
+      const game = new TicTacToe();
+      const error = new Error('Invalid move - Board place already occupied!'); 
+      advanceGameToState(game, gameStates[0]);
+      
+      assert.throws(
+        () => { game.nextMove(0, 0) },
+        error
+      )
+    })
+  });
+
+  describe('The board', () => {
+    it('creates an empty board', () => {
+      const game = new TicTacToe();
+
+      game.board.flat().forEach((el) => {
+        assert.equal(el, '#');
       });
+    });
+
+    it('Places a nought at the correct position', () => {
+      const game = new TicTacToe();
+      advanceGameToState(game, gameStates[0]);
+
+      assert.strictEqual(game.board[0][0], 'O');
+    });
+
+    it('Places a cross at the correct position', () => {
+      const game = new TicTacToe();
+      advanceGameToState(game, gameStates[0]);
+
+      assert.strictEqual(game.board[0][1], 'X');
     });
   });
 });
